8ba95d8521a725f241a5834d1c71c4f95ec725db
stack <washingtonpraxedes@gmail.com@github.com>
2016-01-26 14:01:59 -0300

    modelagem completa

---
+++ testes2/threads2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct jogador{
+	
+}jogador;
+
+typedef struct deck{
+
+}deck;
+
+int* embaralhar(){
+
+}
+
+void matar_todos(){
+
+}
+
+void sacar_carta(){
+
+}
+
+void descartar(){
+
+}
+
+void verificar_vitoria(){
+
+}
+
+int escolhar_descarte(){
+
+}
+
+int main(){
+
+return 0;
+}
+
