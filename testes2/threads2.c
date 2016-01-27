480f1b217a50704454fad8df9a03146b5fa0e067
Washington <washingtonpraxedes@gmail.com>
2016-01-27 01:10:08 -0300

    embaralhando cartas

--- testes2/threads2.c
+++ testes2/threads2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct jogador{
 	
@@ -9,8 +10,26 @@ typedef struct deck{
 
 }deck;
 
-int* embaralhar(){
-
+/*int*/void embaralhar(){
+	srand((unsigned)time(NULL));
+	int i, vtr1[24],aux, j, t;
+	for (i=0;i<24;i++){
+		vtr1[i] = 25;
+	}
+	for(i=0;i<24;i++){
+		t=1;
+		do{
+			aux = rand()%24;
+			for(j=0;j<24;j++){
+				if(!(aux == vtr1[j])) t=1; 
+				else {t = 0;break;}
+			}
+			if(t) vtr1[i] = aux;
+		}while(!t);		
+	}
+	printf("deck ");
+	for(i=0;i<24;i++) printf("%d ",vtr1[i]);
+	printf("\n");
 }
 
 void matar_todos(){
@@ -34,6 +53,11 @@ int escolhar_descarte(){
 }
 
 int main(){
+	embaralhar();
+	sleep(2);
+	embaralhar();
+	sleep(2);
+	embaralhar();
 
 return 0;
 }
